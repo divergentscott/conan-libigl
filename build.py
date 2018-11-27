419b7a1fee4265eff0be77f4fc76122844b6e0ae
David Mugnai <dvd@gnx.it>
2018-11-27 14:38:44 +0100

    build: tests also the static_library option

--- build.py
+++ build.py
@@ -1,7 +1,18 @@
 from cpt.packager import ConanMultiPackager
 
+def build_conf(multi_options, **kw):
+    items = []
+    for options in multi_options:
+        builder = ConanMultiPackager()
+        builder.add_common_builds(pure_c=False, shared_option_name=False)
+        builder.update_build_if(lambda x: True, new_options=options)
+        items.extend(builder.items)
+    return items
+
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.items = build_conf(multi_options=[{"libigl:static_library": False}, {"libigl:static_library": True}],
+                               pure_c=False,
+                               shared_options_name=False)
     builder.run()
