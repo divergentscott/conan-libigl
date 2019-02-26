f1f2878eb7c315a0bb7be0c50ecd1645aa95a862
David Mugnai <dvd@gnx.it>
2019-02-26 14:14:09 +0100

    split the ci work on multiple jobs

--- build.py
+++ build.py
@@ -1,3 +1,4 @@
+import os
 from cpt.packager import ConanMultiPackager
 
 
@@ -11,13 +12,39 @@ def build_conf(multi_options, **kw):
     return items
 
 
+def filter_conf(cfg, items):
+    def f(build):
+        if not cfg["build_static"] and build.options["libigl:static_library"]:
+            return False
+        if not cfg["build_libcxx"] and build.settings["compiler.libcxx"] == "libstdc++":
+            return False
+        if not cfg["build_libcxx11"] and build.settings["compiler.libcxx"] in ("libstdc++11", "libc++"):
+            return False
+        return True
+    return filter(f, items)
+
+
 if __name__ == "__main__":
+    cfg = {
+        "build_static": False,
+        "build_libcxx11": False,
+        "build_libcxx": False,
+    }
+
+    env = os.environ
+    if env.get("PACKAGE_BUILD_STATIC_LIB") == "1":
+        cfg["build_static"] = True
+        cfg["build_libcxx11"] = env.get("PACKAGE_BUILD_LIBCXX11_ABI") == "1"
+        cfg["build_libcxx"] = env.get("PACKAGE_BUILD_LIBCXX_ABI") == "1"
+
     options = [
         {"libigl:static_library": False},
         {"libigl:static_library": True},
     ]
     builder = ConanMultiPackager()
-    builder.items = build_conf(multi_options=options,
-                               pure_c=False,
-                               shared_options_name=False)
+    builder.items = filter_conf(
+        cfg,
+        items=build_conf(multi_options=options,
+                         pure_c=False,
+                         shared_options_name=False))
     builder.run()
